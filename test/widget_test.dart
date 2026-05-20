2641228f8d823a764217bd2c741010b6ffae3278
Bethel03 <bethelkens9@gmail.com>
2026-05-20 17:00:23 +0200

    FIX: validate game answers in parallel using Future.wait to optimize speed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:petit_bac/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Petit Bac smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the app's title and play button exist.
+    expect(find.text('Petit Bac'), findsOneWidget);
+    expect(find.text('Jouer'), findsOneWidget);
   });
 }
